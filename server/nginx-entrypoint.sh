82feb69960d5341126901c6337789732ca89bd4c
Sam Gardner <lwnexgen@gmail.com>
2021-10-21 00:38:58 -0500

    Fixing a few lingering issues - now works in both "now" mode and "sched" mode

--- server/nginx-entrypoint.sh
+++ server/nginx-entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-# Wait for letsencrypt container to be up
-wait-for-it letsencrypt:80 || exit 1
+wait_and_retry() {
+    wait-for-it letsencrypt:80 || exit 1
+    certbot -v --non-interactive install -d "$DOMAIN" --cert-name "$DOMAIN" --nginx || exit 1
+}
 
 # initialize certbot - TODO ; should be templatized using Jinja
 sed -e "s;DOMAIN_PLACEHOLDER;$DOMAIN;g" /etc/nginx/conf.d/default.conf -i
 
-# Install domain cert into container
-certbot -v --non-interactive install -d "$DOMAIN" --cert-name "$DOMAIN" --nginx
+# Install domain cert into container or wait until letsencrypt is done with initial install
+certbot -v --non-interactive install -d "$DOMAIN" --cert-name "$DOMAIN" --nginx || wait_and_retry
 
 /usr/sbin/nginx -s stop;
 sleep 10;
 /usr/sbin/nginx -g "daemon off;"
-   
