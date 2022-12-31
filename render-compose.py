aaf5cd3e7a23aaeef5ef89b4c9795dec05bd48de
Sam Gardner <lwnexgen@gmail.com>
2022-12-31 15:17:42 -0600

    Implement a nice demo script

--- render-compose.py
+++ render-compose.py
@@ -7,7 +7,14 @@ import urlparse
 def main():
     config = json.load(open('tuner/config.json'))
     local_ip = urlparse.urlparse(config['local_address']).netloc.split(':')[0]
-    cmd = ["sed", "-e", "s;DEFINEIP;{};g".format(local_ip), 'docker-compose.yml.templ']
+    code_dir = config['code_dir']
+    base_dir = config['base_dir']
+    cmd = ["sed",
+           "-e", "s;DEFINEIP;{};g".format(local_ip),
+           "-e", "s;CODEDIR;{};g".format(code_dir),
+           "-e", "s;BASEDIR;{};g".format(base_dir),
+           'docker-compose.yml.templ'
+    ]
     subprocess.call(cmd)
     
 if __name__ == '__main__':
